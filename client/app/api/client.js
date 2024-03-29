59b8453302f1cd74aa990bcc9796b6570fbd8c92
Jessey Uche-Nwichi <jesseyuchenwichi@gmail.com>
2024-03-29 11:10:28 +0000

    Added API_URL to client

--- client/app/api/client.js
+++ client/app/api/client.js
@@ -3,8 +3,10 @@ import Constants from "expo-constants";
 
 const { expoConfig } = Constants;
 
-export default axios.create({ 
-    baseURL: `http://${expoConfig?.hostUri
-    ? expoConfig.hostUri.split(`:`).shift()
-    : `localhost`}:8000`
+export default axios.create({
+  baseURL:
+    process.env.EXPO_PUBLIC_API_URL ||
+    `http://${
+      expoConfig?.hostUri ? expoConfig.hostUri.split(`:`).shift() : `localhost`
+    }:8000`,
 });
