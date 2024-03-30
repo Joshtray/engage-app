a787486db1470669cb67ccee7c48f5e64dee6742
Jessey Uche-Nwichi <jesseyuchenwichi@gmail.com>
2024-03-30 15:58:23 +0000

    Only retrieve upcoming company activities

--- server/routes/event.js
+++ server/routes/event.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const Event = require("../models/event");
 const { isAuth } = require("../middlewares/auth");
 
-// Get all events for a user's company
+// Get all upcoming events for a user's company
 router.get("/company-events", isAuth, async (req, res) => {
   try {
-    const events = await Event.find({ company: req.user.company });
-    console.log(events);
+    const events = await Event.find({
+      date: { $gte: new Date() },
+      company: req.user.company,
+    }).populate("tags attendees company");
     res.json({ success: true, events });
   } catch (e) {
     res.json({ success: false, message: e.message });
